ff5f76ebd01a2691f1ef33b09b8da41b521a02ab
Heriz <herrypatel3369@gmail.com>
2021-04-10 16:27:37 -0400

    Completed section 14

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import { Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe',
@@ -29,6 +28,10 @@ export class RecipeService {
 
   getRecipe() {
     return this.recipes.slice();
+  }
+
+  getRecipes(index: number){
+    return this.recipes.slice()[index];
   }
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
